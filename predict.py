1cadad4462448f11c9c13b36c2bf39ed5a8aa41d
Olivia Liu <86632849+ojiaxil@users.noreply.github.com>
2024-06-08 19:48:52 -0500

    Generate text function

--- predict.py
+++ predict.py
@@ -15,3 +15,36 @@ def loadWeights(model):
     model.loadWeights(file)
 
     model.compile(loss = 'categorical_crossentropy', optimizer = 'rmsprop')
+
+def generateText(model, raw):
+    chars = sorted(set(raw))
+    char_to_index = {char: idx for idx, char in enumerate(chars)}
+    index_to_char = {idx: char for idx, char in enumerate(chars)}
+
+    total_char = len(raw)
+    vocab_size = len(char)
+
+    seq_len = 100
+
+    input, output = genInputOutputPairs(raw, total_char, char_to_index, seq_len)
+
+    start = np.random.raindint(0, len(input) - 1)
+    pattern = input[start]
+
+    iterations = int(sys.argv[2])
+
+    for i in range(iterations):
+        x = np.reshape(pattern, (1, len(pattern), 1))
+        x = x / float(vocab_size)
+        prediction = model.predict(x, verbose = 0)
+        index = np.argma(prediction)
+        result = index_to_char[index]
+        seq_in = [index_to_char[value] for value in pattern]
+        sys.stdout.write(result)
+        pattern.append(index)
+        pattern = pattern[1:len(pattern)]
+        print("\nDone")
+
+if __name__ == '_main__':
+    file = sys.argv[1]
+    predict(file)
